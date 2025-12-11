bf3200ec490d7a111bc06d4d4ad8fe43960e95f3
Loi-ND <loinguyenduy12a1@gmail.com>
2025-12-11 16:54:15 +0700

    ..

--- src/crawl.py
+++ src/crawl.py
@@ -6,3 +6,5 @@ GLobalConfig.init(GLobalConfig)
 print(f"API_KEY: {GLobalConfig.API_KEY}")
 print(f"Start index: {os.environ.get('START_INDEX')}")
 print(f"End index: {os.environ.get('END_INDEX')}")
+
+pull_data()
