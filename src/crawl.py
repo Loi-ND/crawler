5adf9ac5657617d93c45560396121686899e2674
Loi-ND <loinguyenduy12a1@gmail.com>
2025-12-11 16:45:23 +0700

    hi

--- src/crawl.py
+++ src/crawl.py
@@ -3,5 +3,5 @@ import os
 
 GLobalConfig.init(GLobalConfig)
 print(f"API_KEY: {GLobalConfig.API_KEY}")
-print(f"Start index: {os.environ.get("START_INDEX")}")
-print(f"End index: {os.environ.get("END_INDEX")}")
+print(f"Start index: {os.environ.get('START_INDEX')}")
+print(f"End index: {os.environ.get('END_INDEX')}")
