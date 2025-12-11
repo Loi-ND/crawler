4d06a8f8c048384b08445772295a621b3be16c9a
Loi-ND <loinguyenduy12a1@gmail.com>
2025-12-11 16:02:37 +0700

    update

--- src/config.py
+++ src/config.py
@@ -1,5 +1,6 @@
-API_KEY="xyIya6XwlccEh1viOTJ0ebBY6RmCOix6"
-API_URL="https://api.massive.com/v3/reference/tickers"
-DATA_FOLDER="../labels/data"
-INDEX_PATH="../labels/index.txt"
-TICKERS_PATH="../labels/tickers.json"
+class GLobalConfig:
+    API_KEY="xyIya6XwlccEh1viOTJ0ebBY6RmCOix6"
+    API_URL="https://api.massive.com/v3/reference/tickers"
+    DATA_FOLDER="/kaggle/working/crawler/labels/data"
+    INDEX_PATH="/kaggle/working/crawler/labels/index.txt"
+    TICKERS_PATH="/kaggle/working/crawler/labels/tickers.json"
